995757ad224db25597735f060729ba85cec30964
jun <ssb00134@naver.com>
2021-03-29 22:46:57 +0900

    redandblue

---
+++ src/component/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <ui>
+      <li>
+        <link to="/red">Red</link>
+      </li>
+      <li>
+        <link to="/blue">Blue</link>
+      </li>
+    </ui>
+  );
+};
+
+export default componentName;
